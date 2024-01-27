b16da5a93edec90f2c447c700e1b2acf5c75990a
lucymao-12 <lucymao12@gmail.com>
2024-01-27 13:46:02 -0500

    setup some models, got tests working

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const userSchema = new Schema({});
+const userSchema = new Schema({
+  userID: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  numCompletedCollages: { type: Number, required: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const userController = require("../controllers/user.controller");
+const userController = require("../controllers/userController.js");
 
 const userRouter = express.Router();
 
 // Remember that all api routes start with /api then specific routes like /users/:id
 
 // get all users
-userRouter.get("/users", userController.getAllUsers);
+//userRouter.get("/users", userController.getAllUsers);
 
 // get a specific user from database.
 userRouter.get("/users/:id", userController.getUser);
