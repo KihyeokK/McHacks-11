de08a69d9f80fa2968b7408e92179c21b020b09b
Rei Kong <reiikongs@gmail.com>
2024-01-27 17:35:20 -0500

    Adding bottom text and changing icons

--- frontend/components/BottomNavBar.tsx
+++ frontend/components/BottomNavBar.tsx
@@ -1,114 +1,37 @@
+"use client";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { BsFillGrid1X2Fill, BsFillPeopleFill, BsImages, BsPersonFill } from "react-icons/bs";
 
-const NavBar = () => (
-  <nav className="fixed bottom-0 left-0 right-0 bg-white py-4 border-t">
-    <div className="flex justify-around">
-      <Link href="/collages">
-        <SchoolIcon className="text-blue-500" />
-      </Link>
-      <Link href="/friends">
-        <FacebookIcon className="text-gray-500" />
-      </Link>
-      <Link href="/memory">
-        <MemoryStickIcon className="text-gray-500" />
-      </Link>
-      <Link href="/profile">
-        <UserIcon className="text-gray-500" />
-      </Link>
-    </div>
-  </nav>
-);
-function SchoolIcon(props) {
+const BottomNavBar = () => {
+  const router = useRouter();                                                         // Update the type of the router variable
+
+  if (!router) {
+    return null;                                                                      // handle the case when the router is not available
+  }
+
   return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m4 6 8-4 8 4" />
-      <path d="m18 10 4 2v8a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-8l4-2" />
-      <path d="M14 22v-4a2 2 0 0 0-2-2v0a2 2 0 0 0-2 2v4" />
-      <path d="M18 5v17" />
-      <path d="M6 5v17" />
-      <circle cx="12" cy="9" r="2" />
-    </svg>
-  )
-}
+    <nav className="fixed bottom-0 left-0 right-0 bg-white py-2 border-t">
+      <div className="flex justify-around">
+        <Link href="/collages" className={`flex-col justify-center items-center gap-1.5 inline-flex hover:text-indigo-500 ${router.pathname === '/collages' ? 'text-indigo-500' : 'text-indigo-950'}`}>
+          <BsFillGrid1X2Fill className="text-xl" />
+          <div className="text-sm font-normal font-['Rubik']">Collages</div>
+        </Link>
+        <Link href="/friends" className={`flex-col justify-center items-center gap-1.5 inline-flex hover:text-indigo-500 ${router.pathname === '/friends' ? 'text-indigo-500' : 'text-indigo-950'}`}>
+          <BsFillPeopleFill className="text-2xl" />
+          <div className="text-sm font-normal font-['Rubik']">Friends</div>
+        </Link>
+        <Link href="/memory" className={`flex-col justify-center items-center gap-1.5 inline-flex hover:text-indigo-500 ${router.pathname === '/memory' ? 'text-indigo-500' : 'text-indigo-950'}`}>
+          <BsImages className="text-2xl" />
+          <div className="text-sm font-normal font-['Rubik']">Memories</div>
+        </Link>
+        <Link href="/profile" className={`flex-col justify-center items-center gap-1.5 inline-flex hover:text-indigo-500 ${router.pathname === '/profile' ? 'text-indigo-500' : 'text-indigo-950'}`}>
+          <BsPersonFill className="text-2xl" />
+          <div className="text-sm font-normal font-['Rubik']">Profile</div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-
-function FacebookIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
-    </svg>
-  )
-}
-
-
-function MemoryStickIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M6 19v-3" />
-      <path d="M10 19v-3" />
-      <path d="M14 19v-3" />
-      <path d="M18 19v-3" />
-      <path d="M8 11V9" />
-      <path d="M16 11V9" />
-      <path d="M12 11V9" />
-      <path d="M2 15h20" />
-      <path d="M2 7a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v1.1a2 2 0 0 0 0 3.837V17a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-5.1a2 2 0 0 0 0-3.837Z" />
-    </svg>
-  )
-}
-
-
-function UserIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-      <circle cx="12" cy="7" r="4" />
-    </svg>
-  )
-}
-
-export default NavBar;
+export default BottomNavBar;
