b1dc3f81147b9d63a9f153fb41cabd359f37e593
lucymao-12 <lucymao12@gmail.com>
2024-01-27 23:40:06 -0500

    add images to goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,23 @@
 const express = require("express");
 const Goal = require("../models/Goal.js");
 const goalRouter = express.Router();
+const fs = require("fs");
+const { GridFSBucket } = require("mongodb");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config({ path: path.resolve(__dirname, "../.env") });
+const MONGODB_URI = process.env.MONGODB_URI;
+
+let gfs;
+const conn = mongoose.createConnection(process.env.MONGODB_URI);
+conn.once("open", () => {
+    const db = conn.db;
+    gfs = new GridFSBucket(db, {
+        bucketName: "uploads"
+    });
+});
 
 // create goal
 exports.addGoal = async (req, res) => {
@@ -42,7 +59,22 @@ exports.getAllGoals = async (req, res) => {
 exports.addImage = async (req, res) => {
     try {
         const goal = await Goal.findOne({ goalID: req.params.id });
-        goal.images.push(req.body.image);
+        const imageID = req.body.imageID;
+        const image = await gfs.find({ _id: imageID });
+        goal.images.push(image);
+        res.status(200).json({ goal });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+// remove user from goal
+
+exports.removeUser = async (req, res) => {
+    try {
+        const user = await User.findOne({ userID: req.params.userID });
+        const goal = await Goal.findOne({ goalID: req.params.goalID });
+        goal.users.remove(user);
         res.status(200).json({ goal });
     } catch (err) {
         res.status(400).json({ message: err.message });
