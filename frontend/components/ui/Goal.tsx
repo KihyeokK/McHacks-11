409fd1db636e0c7dec7f76146c849a8af1e375e7
KihyeokK <joe.brighter20@gmail.com>
2024-01-28 02:22:11 -0500

    add base collages page

--- frontend/components/ui/Goal.tsx
+++ frontend/components/ui/Goal.tsx
@@ -1,51 +1,83 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import Image from "next/image";
+import catImage from "../../../public/images/cat.jpg";
 
 type GoalProps = {
-    id: number;
+  id: number;
 };
 
 type GoalData = {
-    name: string;
-    description: string;
-    photos: string[]; // change to the proper data type
-    numUploadedPhotos: number;
-    numPhotos: number;  // use to determine the numebr
-};  
+  name: string;
+  description: string;
+  photos: string[]; // change to the proper data type
+  numUploadedPhotos: number;
+  numPhotos: number; // use to determine the numebr
+};
 
 export default function Goal({ id }: GoalProps) {
-    // const [goalName, setGoalName] = useState();
-    const [goalData, setGoalData] = useState<GoalData | null>(null);
+  // const [goalName, setGoalName] = useState();
+  const [goalData, setGoalData] = useState<GoalData | null>();
 
-    useEffect(() => {
-        const fetchGoalData = async () => {
-            
-        }
-        fetchGoalData;
-        console.log()
-    })
+  useEffect(() => {
+    const fetchGoalData = async () => {};
+    fetchGoalData;
+    console.log();
+  });
 
-    return (
-        <div>
-          <div>
-            {/*{goalData.photos.map((photo, index) => (
+  return (
+    <div className="bg-orange-100 w-100 p-2 mt-3">
+      {/* <div>
+        {goalData.photos.map((photo, index) => (
               <img key={index} src={photo} alt={`Photo ${index + 1}`} />
-            ))}*/}
+            ))}
+      </div> */}
+      <div className="grid grid-cols-2 gap-4 place-items-center">
+        <div className="col-span-1 aspect-w-1 aspect-h-1">
+          <Image
+            src="/images/cat1.png"
+            width={400}
+            height={400}
+            alt="Empty state image"
+          />
+        </div>
+        <div className="aspect-w-1 aspect-h-1">
+          <Image
+            src="/images/cat1.png"
+            width={400}
+            height={400}
+            alt="Empty state image"
+          />
+        </div>
+        <div className="aspect-w-1 aspect-h-1">
+          <Image
+            src="/images/cat1.png"
+            width={400}
+            height={400}
+            alt="Empty state image"
+          />
+        </div>
+        <div className="aspect-w-1 aspect-h-1">
+          <Image
+            src="/images/cat1.png"
+            width={400}
+            height={400}
+            alt="Empty state image"
+          />
+        </div>
+      </div>
+
+      <div>
+        <div className="flex items-center justify-between">
+          <div className="flex-none w-14 h-14 m-4">
+            <div className="font-bold">Group</div>
+            <div className="text-sm">Description</div>
           </div>
-          <div>
-            <div>       {/* Goal Details */}
-                {/*<h2>{goalData.name}</h2>*/}
-                {/*<p>{goalData.description}</p>*/}
-                <h2 className="text-center text-indigo-950 text-lg font-bold font-['Rubik'] leading-[25.20px]">Goal</h2>
-                <p className="text-center text-indigo-950 text-sm font-normal font-['Rubik'] leading-tight">Description</p>
-            </div>
-            <div>   {/* ... */}
-                <BsThreeDotsVertical />
-            </div>
+          <div className="flex-initial w-34 m-3">
+            <BsThreeDotsVertical />
           </div>
         </div>
-    );
-    
-    
-
+      </div>
+    </div>
+  );
 }
